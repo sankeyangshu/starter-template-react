8c89d4f5bf0b3cb6945066d74b0bad079cd6309b
sankeyangshu <1260323835@qq.com>
2025-11-25 09:35:05 +0800

    chore: init

---
+++ eslint.config.ts
@@ -0,0 +1,62 @@
+import antfu from '@antfu/eslint-config';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+import pluginRouter from '@tanstack/eslint-plugin-router';
+import pluginTailwindcss from 'eslint-plugin-better-tailwindcss';
+
+export default antfu(
+  {
+    formatters: true,
+    react: true,
+    typescript: {
+      tsconfigPath: 'tsconfig.json',
+    },
+    stylistic: {
+      indent: 2,
+      quotes: 'single',
+      semi: true,
+    },
+    ignores: ['**/routeTree.gen.ts'],
+    isInEditor: false,
+    rules: {
+      'style/arrow-parens': ['error', 'always'], // 箭头函数参数始终添加括号
+      'style/brace-style': ['error', '1tbs', { allowSingleLine: true }], // 括号样式
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    rules: { 'ts/no-misused-promises': ['error', {
+      checksVoidReturn: {
+        attributes: false,
+      },
+    }] },
+  },
+  {
+    plugins: {
+      'better-tailwindcss': pluginTailwindcss,
+    },
+    rules: {
+      // enable all recommended rules to report a warning
+      ...pluginTailwindcss.configs['recommended-warn'].rules,
+      // enable all recommended rules to report an error
+      ...pluginTailwindcss.configs['recommended-error'].rules,
+
+      // or configure rules individually
+      'better-tailwindcss/enforce-consistent-line-wrapping': ['warn', { printWidth: 100 }],
+    },
+    settings: {
+      'better-tailwindcss': {
+        entryPoint: 'src/styles/global.css',
+      },
+    },
+  },
+  {
+    plugins: {
+      '@tanstack/router': pluginRouter,
+      '@tanstack/query': pluginQuery,
+    },
+    rules: {
+      ...pluginRouter.configs.recommended.rules,
+      ...pluginQuery.configs.recommended.rules,
+    },
+  },
+);
---
+++ vite.config.ts
@@ -0,0 +1,64 @@
+import type { ConfigEnv, UserConfig } from 'vite';
+import process from 'node:process';
+import { fileURLToPath } from 'node:url';
+import { defineConfig, loadEnv } from 'vite';
+import { createProxy, getLastBuildTime, wrapperEnv } from './build/config';
+import { createVitePlugins } from './build/plugins';
+
+// https://vite.dev/config/
+export default defineConfig((config: ConfigEnv): UserConfig => {
+  const root = process.cwd();
+  const { mode } = config;
+
+  const viteEnv = wrapperEnv(loadEnv(mode, root));
+
+  const lastBuildTime = getLastBuildTime();
+
+  return {
+    base: viteEnv.VITE_BASE_URL || '/',
+    root,
+
+    // 加载插件
+    plugins: createVitePlugins(viteEnv, lastBuildTime),
+
+    // 配置路径别名
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+
+    // 跨域代理
+    server: {
+      host: true,
+      open: viteEnv.VITE_OPEN,
+      port: viteEnv.VITE_PORT,
+      proxy: createProxy(viteEnv.VITE_PROXY),
+    },
+
+    // 定义全局常量替换方式
+    define: {
+      BUILD_TIME: JSON.stringify(lastBuildTime),
+    },
+
+    esbuild: {
+      // 使用 esbuild 压缩 剔除 console 和 debugger
+      drop: viteEnv.VITE_DROP_CONSOLE ? ['console', 'debugger'] : [],
+    },
+
+    build: {
+      minify: 'esbuild',
+      sourcemap: false,
+      outDir: viteEnv.VITE_OUTPUT_DIR || 'dist',
+      reportCompressedSize: false,
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
+          entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
+          assetFileNames: '[ext]/[name]-[hash].[ext]', // 资源文件像 字体，图片等
+        },
+      },
+    },
+  };
+});
